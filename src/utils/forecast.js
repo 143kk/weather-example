20c268217428ec568cc1d9074f33b035a4a74922
Your Name <you@example.com>
2020-10-23 21:28:56 +0800

    Add humidity feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,8 @@ const forecast = (longtitude, latitude, callback) => {
             callback(undefined, body.current.weather_descriptions[0] + '. ' + 
                     "It's " + body.current.temperature + 
                     " degrees. It feels like " + 
-                    body.current.feelslike + ' degrees.'
+                    body.current.feelslike + ' degrees. ' + 
+                    'Humidity: ' + body.current.humidity
             );
         }
     })
